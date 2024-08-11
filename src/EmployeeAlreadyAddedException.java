ad72067daeb741d5f1a86346ba273e5e1bb94416
Locku <lockup86@rambler.ru>
2024-08-11 12:31:23 +0500

    HM20/1

---
+++ src/EmployeeAlreadyAddedException.java
@@ -0,0 +1,12 @@
+public class EmployeeAlreadyAddedException extends RuntimeException {
+    public EmployeeAlreadyAddedException() {
+    }
+
+    public EmployeeAlreadyAddedException(String message) {
+        super(message);
+    }
+
+    public EmployeeAlreadyAddedException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
